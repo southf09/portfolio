70f7aa6b15cace8964e8bd8858e3687a2edccb8d
Frank South <frankstonesouth@gmail.com>
2022-07-01 16:40:39 -0400

    Comments added to app.js

--- src/app.js
+++ src/app.js
@@ -1,6 +1,8 @@
 const burgerBtn = document.getElementById('burger');
 const navList = document.getElementById('navList');
 
+
+//Burger open/close
 function toggleBtn() {
     navList.classList.toggle('show')
 }
