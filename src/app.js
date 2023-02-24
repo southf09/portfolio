35dcd8b0a6828cd30205e0856332dec8f53196ba
Frank South <frankstonesouth@gmail.com>
2023-02-24 16:25:58 -0500

    Experimenting with different filters and effects

--- src/app.js
+++ src/app.js
@@ -1,6 +1,17 @@
+//Selectors for burger menu and nav list 
+
 const burgerBtn = document.getElementById('burger');
 const navList = document.querySelector('.nav-list');
 
+//Selectors for sections and About Me button
+const aboutBtn = document.querySelector('.btn');
+const skillSection = document.querySelector('.skills-section');
+const homeSection = document.querySelector('.home-section');
+const projectSection = document.querySelector('.project-section');
+const aboutSection = document.querySelector('.about-section');
+const footerSection = document.querySelector('.footer-section');
+
+const aboutLogo = document.querySelector('#about-logo');
 
 //Burger open/close
 function toggleBtn() {
@@ -8,3 +19,23 @@ function toggleBtn() {
 }
 
 burgerBtn.addEventListener('click', toggleBtn)
+
+function hideSections() {
+    skillSection.classList.toggle('hide');
+    homeSection.classList.toggle('hide');
+    projectSection.classList.toggle('hide');
+    aboutSection.classList.toggle('hide');
+    footerSection.classList.toggle('hide');
+}
+
+aboutBtn.addEventListener('click', hideSections);
+aboutLogo.addEventListener('click', hideSections);
+
+const skillCards = document.querySelectorAll('.skill-card');
+
+
+
+function blurCard() {
+    skillCards.classList.add('blur');
+}
+skillCards.addEventListener('mouseover', blurCard);
